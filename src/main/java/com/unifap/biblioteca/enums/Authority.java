062dbf1a37a3d419551e94d2bee5ed2312e13eba
audreyrdsc <audreyrdsc@gmail.com>
2025-02-27 23:22:29 -0300

    Criado o Enum com as autoridades ADMIN e USER

---
+++ src/main/java/com/unifap/biblioteca/enums/Authority.java
@@ -0,0 +1,20 @@
+package com.unifap.biblioteca.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+public enum Authority {
+
+    ADMIN(1,"Administrador"),
+    USER(2,"Usuário");
+
+    private final Integer code;
+    private final String description;
+
+    Authority(Integer code, String description) {
+        this.code = code;
+        this.description = description;
+    }
+
+}
