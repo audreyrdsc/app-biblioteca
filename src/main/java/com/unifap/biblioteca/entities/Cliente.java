e6028a99e2a57bad488712f78909a4f2c4567ba9
audreyrdsc <audreyrdsc@gmail.com>
2025-03-13 23:26:18 -0300

    Inclusão do atributo email e mapeamento Objeto Relacional com Emprestimo.class

--- src/main/java/com/unifap/biblioteca/entities/Cliente.java
+++ src/main/java/com/unifap/biblioteca/entities/Cliente.java
@@ -1,10 +1,12 @@
 package com.unifap.biblioteca.entities;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
 
 
 @Entity
@@ -17,19 +19,25 @@ public class Cliente {
 	@Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
-	
-  @Column(name = "nome", lenght = 100, nullable = false, unique = true)
+
+  @Column(name = "nome", length = 100, nullable = false, unique = true)
 	private String nome;
-    
+
   @Column(name = "username", length = 100, nullable = false, unique = true)
   private String username;
-	
-	@Column(columnDefinition = "char(11)", nullable = false, updatable = false)
+
+	@Column(name = "cpf", columnDefinition = "char(11)", nullable = false, updatable = false)
 	private String cpf;
 
-	@Column(columnDefinition = "char(14)")
+	@Column(name = "cellprone", columnDefinition = "char(14)")
 	private String cellphone;
 
-	@Column(columnDefinition = "char(13)")
+	@Column(name = "telephone", columnDefinition = "char(13)")
 	private String telephone;
+
+	@Column(name = "email", length = 50, nullable = false)
+	private String email;
+
+	@OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL)
+	private List<Emprestimo> emprestimos;
 }
