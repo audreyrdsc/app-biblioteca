9429b11987c2f0f031dd62cc4066f18f0767025e
audreyrdsc <audreyrdsc@gmail.com>
2025-03-13 23:40:16 -0300

    Criação do Pacote Exceptions e da Classe GlobalExceptionHandler

---
+++ src/main/java/com/unifap/biblioteca/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,8 @@
+package com.unifap.biblioteca.exceptions;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler extends Exception {
+
+}
