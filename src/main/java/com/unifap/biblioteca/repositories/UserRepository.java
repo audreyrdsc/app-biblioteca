c0185e5a857c460250fc3f8cd7cb64646925d6a8
Francisco Neto <neto.fran18@gmail.com>
2025-03-13 22:47:49 -0300

    Update UserRepository.java
    Classe atualizada com o conteudo.

--- src/main/java/com/unifap/biblioteca/repositories/UserRepository.java
+++ src/main/java/com/unifap/biblioteca/repositories/UserRepository.java
@@ -1,1 +1,14 @@
-"c" 
+package com.unifap.biblioteca.repositories;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.unifap.biblioteca.entities.User;
+
+
+
+@Repository
+public interface UserRepository extends JpaRepository<User, long>{
+
+}
