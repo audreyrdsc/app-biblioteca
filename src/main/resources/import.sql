3d46f6ff1dbefef5a67f89697f256928900a72d9
audreyrdsc <audreyrdsc@gmail.com>
2025-03-18 11:09:21 -0300

    Criação do import.sql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO cliente (cpf, nome, nascimento, cellphone, telephone, email) VALUES ('24757689080', 'Almiro José da Silva', '1992-09-02', '96999999999', '9632225432', 'almiro@email.com');
+INSERT INTO cliente (cpf, nome, nascimento, cellphone, telephone, email) VALUES ('37395125045', 'Luana Ramires da Silva', '2000-05-12', '96888888888', '9633334444', 'luana@email.com');
+INSERT INTO cliente (cpf, nome, nascimento, cellphone, telephone, email) VALUES ('45122907021', 'Anderson Lima Rodrigues', '1985-12-03', '96777777777', '9631112222', 'anderson@email.com');
+INSERT INTO cliente (cpf, nome, nascimento, cellphone, telephone, email) VALUES ('48153107003', 'Maria Bastos de Almeida', '2002-07-01', '96666666666', '9638765780', 'maria@email.com');
