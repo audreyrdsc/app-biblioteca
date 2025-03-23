e8b1641f9d30c063919accdd56c83115d39ade79
audreyrdsc <audreyrdsc@gmail.com>
2025-03-23 18:38:48 -0300

    Correção na Exception Handler Global

--- src/main/java/com/unifap/biblioteca/handlers/AdviceExceptionHandler.java
+++ src/main/java/com/unifap/biblioteca/handlers/AdviceExceptionHandler.java
@@ -7,59 +7,35 @@ import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 import com.unifap.biblioteca.exceptions.EntityNotFoundException;
 
-/**
- * Controller advice exception handlers
- * @author Thiago Pinheiro do Nascimento
- * @since 13 mar 2025
- **/
+// Controller advice exception handlers
 @ControllerAdvice
 public class AdviceExceptionHandler {
 
-    /**
-     * Redirection for catching illegal argument exception
-     * @author Thiago Pinheiro do Nascimento
-     * @since 13 mar 2025
-     **/
+    // Redirection for catching illegal argument exception
     @ExceptionHandler(IllegalArgumentException.class)
     public String handleIllegalArgumentException(IllegalArgumentException e) {
         return "errors/400";
     }
 
-    /**
-     * Redirection for catching entity not found exception
-     * @author Thiago Pinheiro do Nascimento
-     * @since 13 mar 2025
-     **/
+    // Redirection for catching entity not found exception
     @ExceptionHandler(EntityNotFoundException.class)
     public String handleEntityNotFound(EntityNotFoundException e) {
         return "errors/404";
     }
 
-    /**
-     * Redirection for catching no handle exception
-     * @author Thiago Pinheiro do Nascimento
-     * @since 13 mar 2025
-     **/
+    // Redirection for catching no handle exception
     @ExceptionHandler(NoHandlerFoundException.class)
     public String handleNoHandleFound(NoHandlerFoundException e) {
         return "errors/404";
     }
 
-    /**
-     * Redirection for catching no resource exception
-     * @author Thiago Pinheiro do Nascimento
-     * @since 13 mar 2025
-     **/
+    // Redirection for catching no resource exception
     @ExceptionHandler(NoResourceFoundException.class)
     public String handleNoResourceFound(NoResourceFoundException e) {
         return "errors/404";
     }
 
-    /**
-     * Redirection for catching exception
-     * @author Thiago Pinheiro do Nascimento
-     * @since 13 mar 2025
-     **/
+    // Redirection for catching exception
     @ExceptionHandler(Exception.class)
     public String handleUncaught(Exception e) {
         return "errors/500";
