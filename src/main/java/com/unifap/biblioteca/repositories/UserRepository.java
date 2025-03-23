6719535cc19467bf078453bfb54caee47d26ae6c
audreyrdsc <audreyrdsc@gmail.com>
2025-03-23 18:45:51 -0300

    Ajustes do repositório de usuário

--- src/main/java/com/unifap/biblioteca/repositories/UserRepository.java
+++ src/main/java/com/unifap/biblioteca/repositories/UserRepository.java
@@ -3,7 +3,7 @@ package com.unifap.biblioteca.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+import java.util.Optional;
 import com.unifap.biblioteca.entities.User;
 
 
@@ -11,4 +11,6 @@ import com.unifap.biblioteca.entities.User;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>{
 
+    //Optional - busca user por username
+    public Optional<User> findByUsername(String username);
 }
