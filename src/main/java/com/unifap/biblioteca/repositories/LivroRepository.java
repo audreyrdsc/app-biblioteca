974978e1c5f47cda2786e4d13deadf4998bf362f
Francisco Neto <neto.fran18@gmail.com>
2025-03-24 10:11:39 -0300

    Update LivroRepository.java
    Atualizações necessarias

--- src/main/java/com/unifap/biblioteca/repositories/LivroRepository.java
+++ src/main/java/com/unifap/biblioteca/repositories/LivroRepository.java
@@ -1,14 +1,12 @@
 package com.unifap.biblioteca.repositories;
 
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.unifap.biblioteca.entities.Livro;
 
-
-
 @Repository
-public interface LivroRepository extends JpaRepository<Livro, Long>{
-
+public interface LivroRepository extends JpaRepository<Livro, Long> {
+    boolean existsByIsbn(String isbn);
+    boolean existsByIsbnAndIdNot(String isbn, Long id);
 }
