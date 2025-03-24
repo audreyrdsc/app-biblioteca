0f96cda5ccf5320cd1301aceca478065f86dc4e4
Francisco Neto <neto.fran18@gmail.com>
2025-03-24 11:32:47 -0300

    Update LivroController.java
    Atualizações necessarias

--- src/main/java/com/unifap/biblioteca/controllers/LivroController.java
+++ src/main/java/com/unifap/biblioteca/controllers/LivroController.java
@@ -1,1 +1,41 @@
-"teste" 
+package com.unifap.biblioteca.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.unifap.biblioteca.entities.Livro;
+import com.unifap.biblioteca.exceptions.EntityInUseException;
+import com.unifap.biblioteca.exceptions.EntityNotFoundException;
+import com.unifap.biblioteca.repositories.LivroRepository;
+
+@Service
+public class LivroService {
+
+    @Autowired
+    private LivroRepository livroRepository;
+
+    @Transactional
+    public Livro persist(Livro livro) {
+        if (livroRepository.existsByIsbnAndIdNot(livro.getIsbn(), livro.getId())) {
+            throw new IllegalArgumentException("Este ISBN já está cadastrado para outro livro.");
+        }
+        return livroRepository.save(livro);
+    }
+
+    public Livro findOrFail(Long id) {
+        return livroRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException(String.format("Livro %d não encontrado", id)));
+    }
+
+    @Transactional
+    public void delete(Livro livro) {
+        try {
+            livroRepository.deleteById(livro.getId());
+            livroRepository.flush();
+        } catch (DataIntegrityViolationException e) {
+            throw new EntityInUseException(String.format("Livro %d em uso", livro.getId()));
+        }
+    }
+}
