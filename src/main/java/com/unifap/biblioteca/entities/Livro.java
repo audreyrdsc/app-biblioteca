40d720e6ff634160ff958b06f079df6f6b7cc2eb
audreyrdsc <audreyrdsc@gmail.com>
2025-03-25 10:09:27 -0300

    Inclusão de validações da Classe Livro

--- src/main/java/com/unifap/biblioteca/entities/Livro.java
+++ src/main/java/com/unifap/biblioteca/entities/Livro.java
@@ -1,12 +1,9 @@
 package com.unifap.biblioteca.entities;
 
-import java.util.Date;
-
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
-
-import java.time.Year;
-import java.util.List;
+import org.hibernate.validator.constraints.Length;
 
 @Entity
 @Table(name = "livros")
@@ -21,15 +18,21 @@ public class Livro {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "ISBN é obrigatório")
     @Column(name = "isbn", columnDefinition = "char(15)", nullable = false, unique = true, updatable = false)
     private String isbn;
 
+    @NotBlank(message = "Título é obrigatório")
+    @Length(min = 3, max = 200, message = "Título deve conter entre 3 e 200 caracteres")
     @Column(name = "titulo", nullable = false, unique = true, length = 200)
     private String titulo;
 
+    @NotBlank(message = "Autores é obrigatório")
+    @Length(min = 3, max = 200, message = "Autores deve conter entre 3 e 200 caracteres")
     @Column(name = "autores", nullable = false, length = 100)
     private String autores;
 
+    @NotBlank(message = "Editora é obrigatório")
     @Column(name = "editora", nullable = false, length = 100)
     private String editora;
 
@@ -42,11 +45,11 @@ public class Livro {
     @Column(name = "faixa_etaria", nullable = false)
     private int faixaEtaria;
 
-    @Column(name = "disponivel", columnDefinition = "tinyint(1) default 1", nullable = false)
-    private boolean disponivel;
-
     @Column(name = "ano_publicacao", columnDefinition = "char(4)")
     private String anoDePublicacao;
+
+    @Column(name = "disponivel", columnDefinition = "tinyint(1) default 1", nullable = false)
+    private boolean disponivel;
 
     @OneToOne(mappedBy = "livro", cascade = CascadeType.ALL)
     public Emprestimo emprestimo;
