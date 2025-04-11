76569fa5cfa24f954db2c6c7ef77ea4ba169ed1c
audreyrdsc <audreyrdsc@gmail.com>
2025-04-11 00:04:43 -0300

    Implementação no repositório da busca dos livros emprestados por cliente

--- src/main/java/com/unifap/biblioteca/repositories/MovimentacaoRepository.java
+++ src/main/java/com/unifap/biblioteca/repositories/MovimentacaoRepository.java
@@ -2,12 +2,20 @@ package com.unifap.biblioteca.repositories;
 
 
 import com.unifap.biblioteca.entities.Cliente;
+import com.unifap.biblioteca.entities.Livro;
 import com.unifap.biblioteca.entities.Movimentacao;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 
 @Repository
 public interface MovimentacaoRepository extends JpaRepository<Movimentacao, Long>{
+
+    @Query("SELECT m.livro FROM Movimentacao m WHERE m.cliente.id = :clienteId AND m.dataDevolucao IS NULL")
+    List<Livro> findLivrosEmprestadosAtivosPorCliente(@Param("clienteId") Long clienteId);
 
 }
