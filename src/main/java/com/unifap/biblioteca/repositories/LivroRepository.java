799540c1d50db20bf3a35299474edc13a475e3ba
audreyrdsc <audreyrdsc@gmail.com>
2025-04-11 16:06:55 -0300

    Implementação busca de livro disponível TRUE

--- src/main/java/com/unifap/biblioteca/repositories/LivroRepository.java
+++ src/main/java/com/unifap/biblioteca/repositories/LivroRepository.java
@@ -5,8 +5,11 @@ import org.springframework.stereotype.Repository;
 
 import com.unifap.biblioteca.entities.Livro;
 
+import java.util.List;
+
 @Repository
 public interface LivroRepository extends JpaRepository<Livro, Long> {
     boolean existsByIsbn(String isbn);
     boolean existsByIsbnAndIdNot(String isbn, Long id);
+    List<Livro> findByDisponivelTrue(); // livros disponíveis - não emprestados
 }
